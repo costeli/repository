b0672a7b7e6a1167933e150e6b7f0b95e687a217
costeli <konstantin.csn@gmail.com>
2021-10-14 21:12:01 +0300

    Egor

---
+++ ConsoleApplication111.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+    int n, x = 1;
+    cin >> n;
+    x++;
+    while (n % x == 0) {
+        cout << x;
+        x++;
+        continue;
+    }
+
+    return 0;
+}
